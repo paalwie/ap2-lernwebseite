6d10a5609f3889c307e9ad123a9136c267ef1726
Patrick Wiedenmann <p.wiedenmann@exklusiv.de>
2025-05-14 09:28:38 +0200

    design hinzugefügt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, ContentComponent, FooterComponent, HeaderComponent, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { ContentComponent } from './content/content.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'content', component: ContentComponent
+  },
+  {
+    path: '', redirectTo: '/content', pathMatch: 'full'
+  }
+];
