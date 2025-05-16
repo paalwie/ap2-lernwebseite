70fbfceb4484ae87b3528143ecc04bec2aed4d37
Patrick Wiedenmann <p.wiedenmann@exklusiv.de>
2025-05-16 15:01:25 +0200

    fehlerfunktion hinzugefügt

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, HttpClientModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
+  showForm = false;
+  form = { subject: '', message: '' };
 
+  constructor(private http: HttpClient) {}
+
+  submitForm() {
+    const data = {
+      subject: this.form.subject,
+      message: this.form.message,
+    };
+
+    this.http.post('https://formspree.io/f/mrbqjqkv', data).subscribe(() => {
+      alert('Fehler wurde gesendet.');
+      this.showForm = false;
+      this.form = { subject: '', message: '' };
+    });
+  }
 }
